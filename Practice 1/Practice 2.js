1e144e74b9ad514ba304415dd7a811ebae1f44dc
KevReyes <122305927+KevReyes@users.noreply.github.com>
2023-01-18 13:28:38 -0700

    Add files via upload

---
+++ Practice 1/Practice 2.js
@@ -0,0 +1,6 @@
+function displayText(){
+document.getElementById("myText").innerHTML= "Kevin S, Reyes"
+document.getElementById("myText2").innerHTML= "ksalg026@gmail.com"
+document.getElementById("myText3").innerHTML= "Advertising Arts"
+document.getElementById("myText4").innerHTML= "Graduating in May 2025... maybe"
+}
